b4bf9620aec004fcf4cc12b54ae23fec0e3fd466
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-21 14:00:43 -0600

    POST request implemented to create a board

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -1,3 +1,4 @@
+from app.models import db, Board
 from app.forms.board_form import BoardForm
 from .validation_errors import validation_errors_to_error_messages
 from flask import Blueprint, request
@@ -10,14 +11,21 @@ board_routes = Blueprint('boards', __name__)
 @board_routes.route('/', methods=['POST'])
 @login_required
 def create_board():
+    """
+    Create a new board.
+    """
     form = BoardForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        return {'board': 'success'}
+        board = Board()
+        form.populate_obj(board)
+        db.session.add(board)
+        db.session.commit()
+        return {'board': board.to_dict()}
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
-@board_routes.route('/<int:boardId>')
+@board_routes.route('/<int:boardId>', methods=['GET', 'PATCH', 'DELETE'])
 @login_required
 def board_items(boardId):
     return {'boardItems': 'success'}
