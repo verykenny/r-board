9ef285ca9d426189eba02482fd8d4a23076bed65
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-21 12:07:20 -0600

    ran db migrate and upgrade to confirm model would create correct table in database

--- app/models/board.py
+++ app/models/board.py
@@ -1,9 +1,8 @@
 from .db import db
 
 
-
 class Board(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
