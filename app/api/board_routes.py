353e820e9d2d2221a165012e337234dcb429a12f
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-21 13:47:52 -0600

    added test routes to boards route

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -1,4 +1,17 @@
 from flask import Blueprint
+from flask_login import login_required
 
 
 board_routes = Blueprint('boards', __name__)
+
+
+@board_routes.route('/', methods=['POST'])
+@login_required
+def create_board():
+    return {'board': 'success'}
+
+
+@board_routes.route('/<int:boardId>')
+@login_required
+def board_items(boardId):
+    return {'boardItems': 'success'}
