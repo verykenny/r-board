228dc72b311387abcf36b05062c86f4e6a0749d9
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-22 10:58:15 -0600

    added divs to nav bar to separate out the h1 and links

--- react-app/src/components/Splash/SplashNavBar.js
+++ react-app/src/components/Splash/SplashNavBar.js
@@ -3,31 +3,47 @@ import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import styled from 'styled-components';
 
-export const SplashNav = styled.nav`
+
+const Bar = styled.div`
     background: lightblue;
     display: flex;
     height: 50px;
+    justify-content: center;
+`
+
+const SplashNav = styled.nav`
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: space-between;
+    max-width: 85%;
+    width: 900px;
 `;
+
+const Title = styled.h1`
+`
 
 const SplashNavBar = () => {
     return (
+        <Bar>
         <SplashNav>
+            <Title>rBoard</Title>
+            <div>
+                <NavLink to='/login' exact={true} activeClassName='active'>
+                    Login
+                </NavLink>
 
-                    <NavLink to='/login' exact={true} activeClassName='active'>
-                        Login
-                    </NavLink>
+                <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                    Sign Up
+                </NavLink>
 
-                    <NavLink to='/sign-up' exact={true} activeClassName='active'>
-                        Sign Up
-                    </NavLink>
-
-                    <NavLink to='/login' exact={true} activeClassName='active'>
-                        Demo Login
-                    </NavLink>
-
-                    <LogoutButton />
+                <NavLink to='/login' exact={true} activeClassName='active'>
+                    Demo Login
+                </NavLink>
+            </div>
 
         </SplashNav>
+        </Bar>
     );
 }
 
