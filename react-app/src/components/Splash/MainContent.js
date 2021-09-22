503466a3ee1e3a4dc0eee5cea425a9536f74e23a
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-22 11:16:10 -0600

    updated layout for main content on splash page

--- react-app/src/components/Splash/MainContent.js
+++ react-app/src/components/Splash/MainContent.js
@@ -4,22 +4,38 @@ export const MainContentContainer = styled.main`
     align-items: center;
     display: flex;
     flex: 1 1 auto;
+    flex-wrap: wrap;
     justify-content: space-around;
+    background-image: url('/bg-splash-fridge.jpeg');
+    background-position: center;
+    background-size: cover;
 `;
 
 export const SmallContainer = styled.section`
-    border: 1px solid grey;
-    margin: 20px 50px;
+    border: 1px solid lightgrey;
+    display: flex;
+    flex-direction: column;
+    height: 40%;
+    margin: 0px 50px;
     padding: 20px;
     width: 300px;
+    background: white;
 `;
 
 const MainContent = () => {
     return (
         <>
             <MainContentContainer>
-                <SmallContainer>About App</SmallContainer>
-                <SmallContainer>Sign up log in</SmallContainer>
+                <SmallContainer>
+                    <h2>Welcome to rBoard!</h2>
+                    <p>rBoard meant to combine precious refrigerator space and whiteboards into one app.</p>
+                    <p>Begin by creating a whiteboard and inviting family or roommates to keep your home organized!</p>
+                </SmallContainer>
+                <SmallContainer>
+                    <h2>Get Started!</h2>
+                    <p>Don't have an account yet? Create one by signing up.</p>
+                    <p>Check out the app first by signing in as a demo user!</p>
+                </SmallContainer>
             </MainContentContainer>
 
         </>
