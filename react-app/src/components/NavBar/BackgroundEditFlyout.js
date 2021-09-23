1c8dfc61f90fabfb0d5124179a5d10278ec41f3c
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-23 14:25:44 -0600

    updated background edit flyout

--- react-app/src/components/NavBar/BackgroundEditFlyout.js
+++ react-app/src/components/NavBar/BackgroundEditFlyout.js
@@ -1,18 +1,49 @@
+import { useState } from "react";
 import styled from "styled-components";
-import { FlyOutContainer } from "../StyledComponents";
+import { Button, FlyOutContainer } from "../StyledComponents";
 
 
 
 const EditFlyoutContainer = styled(FlyOutContainer)`
     width: 350px;
+    padding: 40px 20px;
+    padding-left: 70px;
+    justify-content: space-between;
+`;
+
+const BackgroundOptionsContainer = styled.div`
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const SubmitButtonContainer = styled.div`
+    width: 100%;
+    height: fit-content;
+    margin-bottom: 50px;
+    display: flex;
+    justify-content: center;
 `;
 
 
-const BackgroundEditFlyout = ({ setOptionsToggle }) => {
+const BackgroundEditFlyout = ({ setOptionsToggle, board, setBackgroundEditToggle }) => {
+    const [backgroundUrl, setBackgroundUrl] = useState(board.backgrounUrl)
+
+    const handleUpdateBackground = () => {
+        setBackgroundEditToggle(prev => !prev)
+        return;
+    }
 
     return (
         <EditFlyoutContainer>
+            <BackgroundOptionsContainer>
+                options
+            </BackgroundOptionsContainer>
 
+        <SubmitButtonContainer>
+            <Button disabled={board.backgroundUrl === backgroundUrl} onClick={handleUpdateBackground}>Update Background</Button>
+        </SubmitButtonContainer>
         </EditFlyoutContainer>
     )
 }
