0d90dda3f0fcef6fbdf83f493e5d1e21f2624d9b
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-24 15:11:40 -0600

    extracted the checkbox from the rest of the component

--- react-app/src/components/DisplayBoardData/ToDoList/ToDo.js
+++ react-app/src/components/DisplayBoardData/ToDoList/ToDo.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import useBoardType from "../../../context/Board";
+import ToDoCheckBox from "./ToDoCheckBox";
 
 
 const ToDoContainer = styled.div`
@@ -10,53 +11,13 @@ const ToDoContainer = styled.div`
 
 
 function ToDo({ todo, todoListId }) {
-    const { setDisplayBoardData, displayBoardData } = useBoardType()
+    const { setDisplayBoardData } = useBoardType()
 
-    const handleTodoToggle = () => {
-        (async () => {
-            const response = await fetch(`/api/todo_list_items/${todo.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    completed: !todo.completed
-                })
-            })
-            const data = await response.json()
-            if (data.errors) {
-                console.log(data.errors);
-            }
-            setDisplayBoardData(prev => {
-                const updatedBoardItems = { ...prev }
-                updatedBoardItems.todoLists = updatedBoardItems.todoLists.map(todoList => {
-                    if (todoList.id === todoListId) {
-                        const updatedTodoList = {...todoList}
-                        updatedTodoList.todos = updatedTodoList.todos.map(listedTodo => {
-                            if (listedTodo.id === todo.id) {
-                                return data.todo;
-                            } else {
-                                return listedTodo;
-                            }
-                        })
-                        return updatedTodoList
-                    } else {
-                        return todoList;
-                    }
-                })
-                return updatedBoardItems
-            })
-
-        })()
-    }
 
 
     return (
         <ToDoContainer>
-            <i
-                className={`far ${(todo.completed) ? 'fa-check-square' : 'fa-square'}`}
-                onClick={handleTodoToggle}
-            />
+            <ToDoCheckBox todo={todo} todoListId={todoListId}/>
             {todo.content}
         </ToDoContainer>
     )
