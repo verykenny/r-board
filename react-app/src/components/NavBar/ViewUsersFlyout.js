e0c410ef3b3dfbe1de3fe7001e390f753458d1d7
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-30 11:20:40 -0600

    added click away for the flyout

--- react-app/src/components/NavBar/ViewUsersFlyout.js
+++ react-app/src/components/NavBar/ViewUsersFlyout.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { FlyOutContainer } from "../StyledComponents";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
+import { Button } from "../StyledComponents";
 
 const ViewUsersFlyoutContainer = styled(FlyOutContainer)`
     width: 350px;
@@ -12,6 +13,9 @@ const ViewUsersFlyoutContainer = styled(FlyOutContainer)`
 const BoardUserContainer = styled.div`
     color: white;
     margin-bottom: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 const UsersConatiner = styled.div`
@@ -22,10 +26,42 @@ const SectionTitle = styled.h2`
     margin-bottom: 10px;
 `;
 
+const SideMenuOption = styled(Button)`
+    margin: 5px;
+    background: transparent;
+    border: none;
+
+    &:hover {
+        color: #2D75FC;
+    }
+`;
+
+const SideMenuDeleteButton = styled(SideMenuOption)`
+    &:hover {
+        color: #F06449;
+    }
+`;
 
 function ViewUsersFlyout({ board, setViewUsersToggle }) {
     const [boardUsers, setBoardUsers] = useState(null)
     const user = useSelector(state => state.session.user)
+
+    const clickCheck = useRef(null)
+    const ClickChecker = (ref) => {
+        useEffect(() => {
+            const handleCloseOptions = (e) => {
+                if (ref.current && !ref.current.contains(e.target)) {
+                    setViewUsersToggle(prev => !prev)
+
+                }
+            }
+            document.addEventListener("mousedown", handleCloseOptions);
+            return () => {
+                document.removeEventListener("mousedown", handleCloseOptions);
+            };
+        })
+    }
+    ClickChecker(clickCheck)
 
 
     useEffect(() => {
@@ -44,7 +80,7 @@ function ViewUsersFlyout({ board, setViewUsersToggle }) {
 
 
     return (
-        <ViewUsersFlyoutContainer>
+        <ViewUsersFlyoutContainer ref={clickCheck}>
             <UsersConatiner>
                 <SectionTitle>Granted Access</SectionTitle>
                 {boardUsers && boardUsers.filter(boardUser => boardUser.verified === true && boardUser.user.id !== user.id).map(boardUser => (
@@ -67,6 +103,12 @@ function BoardUser({ boardUser }) {
     return (
         <BoardUserContainer>
             {boardUser.user.username}
+            {boardUser.verified && (
+                <SideMenuDeleteButton>Remove</SideMenuDeleteButton>
+            )}
+            {!boardUser.verified && (
+                <SideMenuOption>Approve</SideMenuOption>
+            )}
         </BoardUserContainer>
     )
 }
